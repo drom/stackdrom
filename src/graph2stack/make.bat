02a043864b0cd4cf67687baff3b1cb58614549dd
alex.drom@gmail.com <alex.drom@gmail.com@f5f1e656-efff-1fc8-c5fe-b10cb62728fa>
2012-07-19 16:56:59 +0000

    png images added

--- src/graph2stack/make.bat
+++ src/graph2stack/make.bat
@@ -11,4 +11,12 @@ dot -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\04.svg dot\04.dot
 dot -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\05.svg dot\05.dot
 dot -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\06.svg dot\06.dot
 
+dot -Tpng -Nfontname=Helvetica -Efontname=Helvetica -odoc\00.png dot\00.dot
+dot -Tpng -Nfontname=Helvetica -Efontname=Helvetica -odoc\01.png dot\01.dot
+dot -Tpng -Nfontname=Helvetica -Efontname=Helvetica -odoc\02.png dot\02.dot
+dot -Tpng -Nfontname=Helvetica -Efontname=Helvetica -odoc\03.png dot\03.dot
+dot -Tpng -Nfontname=Helvetica -Efontname=Helvetica -odoc\04.png dot\04.dot
+dot -Tpng -Nfontname=Helvetica -Efontname=Helvetica -odoc\05.png dot\05.dot
+dot -Tpng -Nfontname=Helvetica -Efontname=Helvetica -odoc\06.png dot\06.dot
+
 neato -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\map.svg dot\map.dot
