2d713f68b815d000000eea5546058cb462ad938c
alex.drom@gmail.com <alex.drom@gmail.com@f5f1e656-efff-1fc8-c5fe-b10cb62728fa>
2012-07-27 08:01:59 +0000

    first draft of SVFIG foils

--- trunk/src/graph2stack/make.bat
+++ trunk/src/graph2stack/make.bat
@@ -1,22 +1,14 @@
-%0\
-cd %0\..
-cd /d %0\..
+@%0\
+@cd %0\..
+@cd /d %0\..
 
-perl radix8.pl
-dot -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\00.svg dot\00.dot
-dot -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\01.svg dot\01.dot
-dot -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\02.svg dot\02.dot
-dot -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\03.svg dot\03.dot
-dot -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\04.svg dot\04.dot
-dot -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\05.svg dot\05.dot
-dot -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\06.svg dot\06.dot
+@perl radix8.pl
+@dot -Gbgcolor="transparent" -Ecolor="white" -Estyle="bold" -Efontcolor="yellow" -Ncolor="white" -Nfontcolor="white" -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\00.svg dot\00.dot
+@dot -Gbgcolor="transparent" -Ecolor="white" -Estyle="bold" -Efontcolor="yellow" -Ncolor="white" -Nfontcolor="white" -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\01.svg dot\01.dot
+@dot -Gbgcolor="transparent" -Ecolor="white" -Estyle="bold" -Efontcolor="yellow" -Ncolor="white" -Nfontcolor="white" -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\02.svg dot\02.dot
+@dot -Gbgcolor="transparent" -Ecolor="white" -Estyle="bold" -Efontcolor="yellow" -Ncolor="white" -Nfontcolor="white" -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\03.svg dot\03.dot
+@dot -Gbgcolor="transparent" -Ecolor="white" -Estyle="bold" -Efontcolor="yellow" -Ncolor="white" -Nfontcolor="white" -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\04.svg dot\04.dot
+@dot -Gbgcolor="transparent" -Ecolor="white" -Estyle="bold" -Efontcolor="yellow" -Ncolor="white" -Nfontcolor="white" -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\05.svg dot\05.dot
+@dot -Gbgcolor="transparent" -Ecolor="white" -Estyle="bold" -Efontcolor="yellow" -Ncolor="white" -Nfontcolor="white" -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\06.svg dot\06.dot
 
-dot -Tpng -Nfontname=Helvetica -Efontname=Helvetica -odoc\00.png dot\00.dot
-dot -Tpng -Nfontname=Helvetica -Efontname=Helvetica -odoc\01.png dot\01.dot
-dot -Tpng -Nfontname=Helvetica -Efontname=Helvetica -odoc\02.png dot\02.dot
-dot -Tpng -Nfontname=Helvetica -Efontname=Helvetica -odoc\03.png dot\03.dot
-dot -Tpng -Nfontname=Helvetica -Efontname=Helvetica -odoc\04.png dot\04.dot
-dot -Tpng -Nfontname=Helvetica -Efontname=Helvetica -odoc\05.png dot\05.dot
-dot -Tpng -Nfontname=Helvetica -Efontname=Helvetica -odoc\06.png dot\06.dot
-
-neato -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\map.svg dot\map.dot
+@neato -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\map.svg dot\map.dot
