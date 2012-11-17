8b5b47e124a92e3b03a802b2835a073d0e5f47a2
alex.drom@gmail.com <alex.drom@gmail.com@f5f1e656-efff-1fc8-c5fe-b10cb62728fa>
2012-11-17 07:45:36 +0000

    Forth Day update

--- src/graph2stack/make.bat
+++ src/graph2stack/make.bat
@@ -3,12 +3,12 @@
 @cd /d %0\..
 
 @perl radix8.pl
-@dot -Gbgcolor="transparent" -Ecolor="white" -Estyle="bold" -Efontcolor="yellow" -Ncolor="white" -Nfontcolor="white" -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\00.svg dot\00.dot
-@dot -Gbgcolor="transparent" -Ecolor="white" -Estyle="bold" -Efontcolor="yellow" -Ncolor="white" -Nfontcolor="white" -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\01.svg dot\01.dot
-@dot -Gbgcolor="transparent" -Ecolor="white" -Estyle="bold" -Efontcolor="yellow" -Ncolor="white" -Nfontcolor="white" -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\02.svg dot\02.dot
-@dot -Gbgcolor="transparent" -Ecolor="white" -Estyle="bold" -Efontcolor="yellow" -Ncolor="white" -Nfontcolor="white" -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\03.svg dot\03.dot
-@dot -Gbgcolor="transparent" -Ecolor="white" -Estyle="bold" -Efontcolor="yellow" -Ncolor="white" -Nfontcolor="white" -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\04.svg dot\04.dot
-@dot -Gbgcolor="transparent" -Ecolor="white" -Estyle="bold" -Efontcolor="yellow" -Ncolor="white" -Nfontcolor="white" -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\05.svg dot\05.dot
-@dot -Gbgcolor="transparent" -Ecolor="white" -Estyle="bold" -Efontcolor="yellow" -Ncolor="white" -Nfontcolor="white" -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\06.svg dot\06.dot
+@dot   -Estyle="bold" -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\00.svg dot\00.dot
+@dot   -Estyle="bold" -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\01.svg dot\01.dot
+@dot   -Estyle="bold" -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\02.svg dot\02.dot
+@dot   -Estyle="bold" -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\03.svg dot\03.dot
+@dot   -Estyle="bold" -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\04.svg dot\04.dot
+@dot   -Estyle="bold" -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\05.svg dot\05.dot
+@dot   -Estyle="bold" -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\06.svg dot\06.dot
 
-@neato -Gbgcolor="transparent" -Ecolor="white" -Estyle="bold" -Efontcolor="yellow" -Ncolor="white" -Nfontcolor="white" -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\map.svg dot\map.dot
+@neato -Estyle="bold" -Tsvg -Nfontname=Helvetica -Efontname=Helvetica -odoc\map.svg dot\map.dot
